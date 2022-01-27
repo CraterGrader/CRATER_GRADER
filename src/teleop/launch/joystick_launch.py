d374e4c7f57026fd37711030efdd97523d675ddb
Russell Wong <russwong89@gmail.com>
2022-01-27 17:53:59 -0500

    Created teleop package and joystick launch file

---
+++ src/teleop/launch/joystick_launch.py
@@ -0,0 +1,11 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='joy',
+            executable='joy_node',
+            name='joy_node'
+        )
+    ])
