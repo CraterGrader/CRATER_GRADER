2f8aa28f3f3228a4a878f8bdb6d2f4317df77871
Russell Wong <russwong89@gmail.com>
2022-01-28 13:41:55 -0500

    Created actuator cmd publisher and updated actuator cmd message build with proper dependencies

--- src/teleop/src/teleop_node.cpp
+++ src/teleop/src/teleop_node.cpp
@@ -5,9 +5,23 @@ namespace teleop {
 
 TeleopNode::TeleopNode() : Node("teleop_node") {
   // Initialize publishers and subscribers
+  cmd_pub_ = this->create_publisher<cg_msgs::msg::ActuatorCommand>(
+    "/actuator_cmd", 1
+  );
   joy_sub_ = this->create_subscription<sensor_msgs::msg::Joy>(
     "/joy", 1, std::bind(&TeleopNode::joyCallback, this, std::placeholders::_1)
   );
+  timer_ = this->create_wall_timer(
+    std::chrono::milliseconds(100),
+    std::bind(&TeleopNode::timerCallback, this)
+  );
+}
+
+void TeleopNode::timerCallback() {
+  auto cmd_msg = cg_msgs::msg::ActuatorCommand();
+  cmd_msg.wheel_velocity = 0;
+  cmd_msg.steer_velocity = 1;
+  cmd_pub_->publish(cmd_msg);
 }
 
 void TeleopNode::joyCallback(const sensor_msgs::msg::Joy::SharedPtr msg) {
