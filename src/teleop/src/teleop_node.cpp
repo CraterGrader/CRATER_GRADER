3e3e355af657fb11ba59ad9c9e3b0d9b0f77bc69
Russell Wong <russwong89@gmail.com>
2022-01-28 12:39:18 -0500

    Added joystick msg subscriber to teleop node

--- src/teleop/src/teleop_node.cpp
+++ src/teleop/src/teleop_node.cpp
@@ -1,1 +1,18 @@
 #include "teleop/teleop_node.hpp"
+
+namespace cg {
+namespace teleop {
+
+TeleopNode::TeleopNode() : Node("teleop_node") {
+  // Initialize publishers and subscribers
+  joy_sub_ = this->create_subscription<sensor_msgs::msg::Joy>(
+    "/joy", 1, std::bind(&TeleopNode::joyCallback, this, std::placeholders::_1)
+  );
+}
+
+void TeleopNode::joyCallback(const sensor_msgs::msg::Joy::SharedPtr msg) const {
+  RCLCPP_INFO(this->get_logger(), "Joy msg received");
+}
+
+}  // namespace teleop
+}  // namespace cg
