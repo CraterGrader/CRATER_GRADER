f6b35aba95cb53235d9da15d56d4e26aaf711e73
Russell Wong <russwong89@gmail.com>
2022-01-31 14:46:33 +0000

    CG-157: Teleop Joystick Command Node (#7)
    * Created cg_msgs package to hold custom messages/interfaces, and added pip dependencies to support ROS idl message format
    
    * Create skeleton teleop node and build it
    
    * Added joystick msg subscriber to teleop node
    
    * Set joystick axis state variables in callback
    
    * Renamed joystick state variables and fixed build
    
    * Corrected scaling comments in ActuatorCommand.idl
    
    * Created actuator cmd publisher and updated actuator cmd message build with proper dependencies
    
    * Add teleop node to launch file
    
    * Properly publish actuator command msg
    
    * Load joystick parameters from ROS2 param server
    
    * Created config file to load teleop joystick parameters
    
    * Commenting and formatting
    
    * Remove comments in CMakeLists and format hpp files
    
    * Move packages to src

--- cg_ws/src/teleop/launch/joystick_launch.py
+++ cg_ws/src/teleop/launch/joystick_launch.py
@@ -1,11 +1,24 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
+    teleop_params =  os.path.join(
+        get_package_share_directory('teleop'),
+        'config',
+        'teleop_node_params.yaml'
+    )
     return LaunchDescription([
         Node(
             package='joy',
             executable='joy_node',
             name='joy_node'
+        ),
+        Node(
+            package='teleop',
+            executable='teleop_node',
+            name='teleop_node',
+            parameters=[teleop_params]
         )
     ])
