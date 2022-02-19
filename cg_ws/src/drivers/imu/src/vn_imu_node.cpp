1db3265564e975243615407a99129c83185c14c7
Russell Wong <russwong89@gmail.com>
2022-02-19 18:55:16 -0500

    Check for existence of IMU device

--- cg_ws/src/drivers/imu/src/vn_imu_node.cpp
+++ cg_ws/src/drivers/imu/src/vn_imu_node.cpp
@@ -5,7 +5,14 @@ namespace imu {
 
 VnImuNode::VnImuNode() : Node("vn_imu_node") {
   // Initialize IMU sensor
-  vs_.connect("/dev/ttyUSB0", 115200);  // TODO parametrize device name and baud rate
+  auto device_name = "/dev/ttyUSB0";
+  try {
+    vs_.connect(device_name, 115200);  // TODO parametrize device name and baud rate
+  } catch (const vn::not_found & e) {
+    RCLCPP_FATAL(this->get_logger(), "VectorNav IMU not found at %s", device_name);
+    throw std::runtime_error("VectorNav IMU not found");
+  }
+  RCLCPP_INFO(this->get_logger(), "Connected to VectorNav IMU at %s", device_name);
 
   // Initialize publishers and subscribers
   imu_pub_ = this->create_publisher<sensor_msgs::msg::Imu>(
