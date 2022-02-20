355f505ee2360f46d050a965c5a37aff57ac40cf
Russell Wong <russwong89@gmail.com>
2022-02-19 19:02:46 -0500

    Load device name from params file and created launch file for imu

--- cg_ws/src/drivers/imu/src/vn_imu_node.cpp
+++ cg_ws/src/drivers/imu/src/vn_imu_node.cpp
@@ -5,14 +5,17 @@ namespace imu {
 
 VnImuNode::VnImuNode() : Node("vn_imu_node") {
   // Initialize IMU sensor
-  auto device_name = "/dev/ttyUSB0";
+  std::string device_name;
+  this->declare_parameter<std::string>("device_name", "/dev/ttyUSB0");
+  this->get_parameter("device_name", device_name);
+
   try {
     vs_.connect(device_name, 115200);  // TODO parametrize device name and baud rate
   } catch (const vn::not_found & e) {
-    RCLCPP_FATAL(this->get_logger(), "VectorNav IMU not found at %s", device_name);
+    RCLCPP_FATAL(this->get_logger(), "VectorNav IMU not found at %s", device_name.c_str());
     throw std::runtime_error("VectorNav IMU not found");
   }
-  RCLCPP_INFO(this->get_logger(), "Connected to VectorNav IMU at %s", device_name);
+  RCLCPP_INFO(this->get_logger(), "Connected to VectorNav IMU at %s", device_name.c_str());
 
   // Initialize publishers and subscribers
   imu_pub_ = this->create_publisher<sensor_msgs::msg::Imu>(
