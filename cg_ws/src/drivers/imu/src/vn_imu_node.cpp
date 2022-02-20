df15104ec4cdd960327b804d5c6723fca8b9e700
Russell Wong <russwong89@gmail.com>
2022-02-19 19:04:56 -0500

    Add baud rate as a param

--- cg_ws/src/drivers/imu/src/vn_imu_node.cpp
+++ cg_ws/src/drivers/imu/src/vn_imu_node.cpp
@@ -8,14 +8,16 @@ VnImuNode::VnImuNode() : Node("vn_imu_node") {
   std::string device_name;
   this->declare_parameter<std::string>("device_name", "/dev/ttyUSB0");
   this->get_parameter("device_name", device_name);
-
+  int baud_rate;
+  this->declare_parameter<int>("baud_rate", 115200);
+  this->get_parameter("baud_rate", baud_rate);
   try {
-    vs_.connect(device_name, 115200);  // TODO parametrize device name and baud rate
+    vs_.connect(device_name, baud_rate);  // TODO parametrize device name and baud rate
   } catch (const vn::not_found & e) {
     RCLCPP_FATAL(this->get_logger(), "VectorNav IMU not found at %s", device_name.c_str());
     throw std::runtime_error("VectorNav IMU not found");
   }
-  RCLCPP_INFO(this->get_logger(), "Connected to VectorNav IMU at %s", device_name.c_str());
+  RCLCPP_INFO(this->get_logger(), "Connected to VectorNav IMU at %s with baud rate %d", device_name.c_str(), baud_rate);
 
   // Initialize publishers and subscribers
   imu_pub_ = this->create_publisher<sensor_msgs::msg::Imu>(
