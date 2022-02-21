fb257f30c77758bf6ed4d670bf72cce52357f6a6
Russell Wong <russwong89@gmail.com>
2022-02-21 12:27:52 -0500

    Conditionally add RViz to the launch file if publish_viz is set to true, and set the default value for publish_viz to be false

--- cg_ws/src/drivers/imu/launch/imu_launch.py
+++ cg_ws/src/drivers/imu/launch/imu_launch.py
@@ -5,17 +5,32 @@ import os
 import yaml
 
 def generate_launch_description():
-    imu_params =  os.path.join(
-        get_package_share_directory('imu'),
-        'config',
-        'imu_node_params.yaml'
-    )
+  imu_params_path =  os.path.join(
+    get_package_share_directory('imu'),
+    'config',
+    'imu_node_params.yaml'
+  )
 
-    return LaunchDescription([
-        Node(
-            package='imu',
-            executable='imu_node',
-            name='imu_node',
-            parameters=[imu_params]
-        )
-    ])
+  with open(imu_params_path, 'r') as stream:
+    imu_params = yaml.load(
+    stream,
+    Loader=yaml.SafeLoader
+  )
+
+  nodes = [
+    Node(
+      package='imu',
+      executable='imu_node',
+      name='imu_node',
+      parameters=[imu_params_path]
+    )
+  ]
+
+  if imu_params['imu_node']['ros__parameters']['publish_viz']:
+    nodes.append(Node(
+      package='rviz2',
+      executable='rviz2',
+      name='rviz2'
+    ))
+
+  return LaunchDescription(nodes)
