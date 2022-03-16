3607b11dcd2e46a31320614fa4d4a02447d42cf1
Russell Wong <russwong89@gmail.com>
2022-03-15 21:19:37 -0400

    Changed topic name for consistency with robot_localization

--- cg_ws/src/drivers/imu/src/vn_imu_node.cpp
+++ cg_ws/src/drivers/imu/src/vn_imu_node.cpp
@@ -40,7 +40,7 @@ VnImuNode::VnImuNode() : Node("vn_imu_node") {
 
   // Initialize publishers and subscribers
   imu_pub_ = this->create_publisher<sensor_msgs::msg::Imu>(
-    "/imu", 1
+    "/imu/data", 1
   );
   timer_ = this->create_wall_timer(
     std::chrono::milliseconds(1000/freq),
