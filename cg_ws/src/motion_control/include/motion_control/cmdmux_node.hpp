d12b2ae407dbebb6e93dd52c1902c71ac7f56e59
Alex Pletta <alexpletta@gmail.com>
2022-03-21 20:02:08 -0400

    create core functions and variables for command multiplexer

--- cg_ws/src/motion_control/include/motion_control/cmdmux_node.hpp
+++ cg_ws/src/motion_control/include/motion_control/cmdmux_node.hpp
@@ -1,6 +1,10 @@
 #pragma once 
 
 #include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/int8.hpp>
+#include <cg_msgs/msg/actuator_command.hpp>
+
+// TODO: rename /teleop_cmd topic
 
 namespace cg {
 namespace cmdmux {
@@ -12,11 +16,26 @@ public:
 
 private: 
   /* Publishers and Subscribers */
+  rclcpp::Publisher<cg_msgs::msg::ActuatorCommand>::SharedPtr cmd_pub_;
+
+  rclcpp::Subscription<std_msgs::msg::Int8>::SharedPtr mux_sub_;
+  rclcpp::Subscription<cg_msgs::msg::ActuatorCommand>::SharedPtr teleop_sub_;
+  rclcpp::Subscription<cg_msgs::msg::ActuatorCommand>::SharedPtr autonomy_sub_;
+
+  /* Message data */
+  cg_msgs::msg::Actuatorommand actuator_cmd_;
 
   /* Callbacks */
+  void modeCallback(const std_msgs::msg::Int8::SharedPtr msg);
+  void teleopCallback(const cg_msgs::msg::ActuatorCommand::SharedPtr msg);
+  void autonomyCallback(const cg_msgs::msg::ActuatorCommand::SharedPtr msg);
 
   /* Variables */
-
+  uint8_t curr_mode_;
+  uint8_t idle_mode_;
+  uint8_t autograder_mode_;
+  uint8_t full_autonomy_mode_;
+  uint8_t full_teleop_mode_;
 
 } // class CmdMuxNode
 
--- cg_ws/src/motion_control/src/cmd_mux.cpp
+++ cg_ws/src/motion_control/src/cmd_mux.cpp
@@ -4,10 +4,68 @@ namespace cg {
 namespace cmdmux {
 
 CmdMuxNode::CmdMuxNode() : Node("cmd_mux") {
-  // Initialize publishers and subscribers
+  /* Initialize publishers and subscribers */
+  // Actuator command to Arduino
+  cmd_pub_ = this->create_publisher<cg_msgs::msg::ActuatorCommand>(
+      "/actuator_cmd", 1);
 
-  // Load parameters 
+  // Multiplexer mode number
+  mux_sub_ = this->create_subscription<std_msgs::msg::Int8>(
+      "/mux_mode", 1, std::bind(&CmdMuxNode::muxCallback, this, std::placeholders::_1));
 
+  // Teleop and autonomy control messages
+  teleop_sub_ = this->create_subscription<cg_msgs::msg::ActuatorCommand>(
+      "/teleop_cmd", 1, std::bind(&CmdMuxNode::teleopCallback, this, std::placeholders::_1));
+  autonomy_sub_ = this->create_subscription<cg_msgs::msg::ActuatorCommand>(
+      "/autonomy_cmd", 1, std::bind(&CmdMuxNode::autonomyCallback, this, std::placeholders::_1));
+
+  
+  /* Load parameters */
+  // Initialize the currenet mode to the default mode
+  this->declare_parameter<int>("default_mode", 0);
+  this->get_parameter("default_mode", curr_mode_);
+
+  // Set values for other modes based on config file
+  this->declare_parameter<int>("idle_mode", 0);
+  this->get_parameter("idle_mode", idle_mode_);
+  this->declare_parameter<int>("autograder_mode", 1);
+  this->get_parameter("autograder_mode", autograder_mode_);
+  this->declare_parameter<int>("full_autonomy_mode", 2);
+  this->get_parameter("full_autonomy_mode", full_autonomy_mode_);
+  this->declare_parameter<int>("full_teleop_mode", 3);
+  this->get_parameter("full_teleop_mode", full_teleop_mode_);
+}
+
+void modeCallback(const std_msgs::msg::Int8::SharedPtr msg) {
+  // Set the current mode using the incoming mode number
+  curr_mode_ = msg.data;
+}
+
+void teleopCallback(const cg_msgs::msg::ActuatorCommand::SharedPtr msg) {
+
+  switch (curr_mode_) {
+    case idle_mode_:
+      // Fix to last command
+      // TODO: set these values correctly
+      actuator_cmd_.wheel_velocity = 0;
+      actuator_cmd_.steer_position = 0;
+      actuator_cmd_.tool_position = 0;
+      break;
+    case autograder_mode_:
+      break;
+    case full_autonomy_mode_:
+      break;
+    case full_teleop_mode_:
+      break;
+    default:
+      // Report the current mode
+  }
+
+}
+
+void autonomyCallback(const cg_msgs::msg::ActuatorCommand::SharedPtr msg)
+{
+  return;
 }
 
 } // namespace cmdmux
