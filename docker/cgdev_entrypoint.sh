acc06d6ee06e600602dee95366e97aaae861efee
Alex Pletta <alexpletta@gmail.com>
2022-03-30 01:54:50 -0400

    prep for making a buildx image

--- docker/cgdev_entrypoint.sh
+++ docker/cgdev_entrypoint.sh
@@ -1,33 +1,45 @@
 #!/bin/zsh
 set -e
 
-# Source ROS underlay binaries
+# -------- ROS and MicroROS -------------------------------
+# Source ROS underlay
 echo 'source /opt/ros/$ROS_DISTRO/setup.zsh' >> /root/.zshrc
 echo 'source /opt/ros/$ROS_DISTRO/setup.bash' >> /root/.bashrc
 
-# Source microROS overlay packages
-echo 'source /root/microros_ws_autobuild/install/local_setup.zsh' >> /root/.zshrc
-echo 'source /root/microros_ws_autobuild/install/local_setup.bash' >> /root/.bashrc
+# Source MicroROS overlay
+echo 'source /uros_ws/install/local_setup.zsh' >> /root/.zshrc
+echo 'source /uros_ws/install/local_setup.bash' >> /root/.bashrc
 
+# Export MicroROS DDS settings
+if [ "$ROS_LOCALHOST_ONLY" = "1" ] ; then
+    echo 'export FASTRTPS_DEFAULT_PROFILES_FILE=/tmp/disable_fastdds_shm_localhost_only.xml' >> /root/.zshrc
+    echo 'export FASTRTPS_DEFAULT_PROFILES_FILE=/tmp/disable_fastdds_shm_localhost_only.xml' >> /root/.bashrc
+else
+    echo 'export FASTRTPS_DEFAULT_PROFILES_FILE=/tmp/disable_fastdds_shm.xml' >> /root/.zshrc
+    echo 'export FASTRTPS_DEFAULT_PROFILES_FILE=/tmp/disable_fastdds_shm.xml' >> /root/.bashrc
+fi
+# ---------------------------------------------------------
+
+# -------- Setup Shell Environment ------------------------
 # Fix zsh autocomplete for ROS2 packages
 echo 'eval "$(register-python-argcomplete3 ros2)"' >> /root/.zshrc
 echo 'eval "$(register-python-argcomplete3 colcon)"' >> /root/.zshrc
 
-# Source cg_ws overlay packages
-# echo 'source /root/cg_ws_autobuild/install/local_setup.zsh' >> /root/.zshrc
-# echo 'source /root/cg_ws_autobuild/install/local_setup.bash' >> /root/.bashrc
-
-echo 'source /root/CRATER_GRADER/cg_ws/install/local_setup.zsh > /dev/null 2>&1' >> ~/.zshrc
-echo 'source /root/CRATER_GRADER/cg_ws/install/local_setup.bash > /dev/null 2>&1' >> ~/.bashrc
+# Try sourcing cg_ws packages
+echo 'source /root/CRATER_GRADER/cg_ws/install/setup.zsh > /dev/null 2>&1' >> ~/.zshrc
+echo 'source /root/CRATER_GRADER/cg_ws/install/setup.bash > /dev/null 2>&1' >> ~/.bashrc
 
 # Start new shells in the cg conda environment
 echo "conda activate cg" >> ~/.zshrc
 echo "conda activate cg" >> ~/.bashrc
+# ---------------------------------------------------------
 
+# -------- Last Setup -------------------------------------
 # Welcome message
 echo "figlet -f slant 'CraterGrader'" >> ~/.zshrc
 
-# Start the VNC server, in shared mode so more than one user can access
+# Start the VNC server, in shared mode so more than one user can access, must be last command to run
 x11vnc -forever -usepw -create -shared
+# ---------------------------------------------------------
 
 exec "$@"
