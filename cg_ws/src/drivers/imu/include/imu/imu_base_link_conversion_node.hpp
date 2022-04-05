e9d2a496418886de1df8b86c009584de83b19740
Russell Wong <russwong89@gmail.com>
2022-04-05 13:57:41 -0400

    Skeleton node for imu_link -> base_link conversion

---
+++ cg_ws/src/drivers/imu/include/imu/imu_base_link_conversion_node.hpp
@@ -0,0 +1,30 @@
+#ifndef IMU__IMU_BASE_LINK_CONVERSION_NODE_HPP
+#define IMU__IMU_BASE_LINK_CONVERSION_NODE_HPP
+
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/imu.hpp>
+
+namespace cg {
+namespace imu {
+
+class ImuBaseLinkConversionNode : public rclcpp::Node {
+
+public:
+  ImuBaseLinkConversionNode();
+
+private:
+  /* Publishers and Subscribers */
+  rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr base_link_imu_pub_;
+  rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_link_imu_sub_;
+  rclcpp::TimerBase::SharedPtr timer_;
+
+  /* Callbacks */
+  void imuLinkImuCallback(const sensor_msgs::msg::Imu::SharedPtr msg);
+  void timerCallback();
+};
+
+
+}  // namespace imu
+}  // namespace cg
+
+#endif  // IMU__IMU_BASE_LINK_CONVERSION_NODE_HPP
---
+++ cg_ws/src/drivers/imu/src/imu_base_link_conversion_node.cpp
@@ -0,0 +1,29 @@
+#include "imu/imu_base_link_conversion_node.hpp"
+
+namespace cg {
+namespace imu {
+
+ImuBaseLinkConversionNode::ImuBaseLinkConversionNode() : Node("imu_base_link_conversion_node") {
+  // Initialize publishers and subscribers
+  base_link_imu_pub_ = this->create_publisher<sensor_msgs::msg::Imu>(
+    "/imu/data/base_link", 1
+  );
+  imu_link_imu_sub_ = this->create_subscription<sensor_msgs::msg::Imu>(
+    "/imu/data/imu_link", rclcpp::SensorDataQoS(), std::bind(&ImuBaseLinkConversionNode::imuLinkImuCallback, this, std::placeholders::_1)
+  );
+  timer_ = this->create_wall_timer(
+    std::chrono::milliseconds(50),
+    std::bind(&ImuBaseLinkConversionNode::timerCallback, this)
+  );
+}
+
+void ImuBaseLinkConversionNode::imuLinkImuCallback(const sensor_msgs::msg::Imu::SharedPtr msg) {
+  // TODO
+}
+
+void ImuBaseLinkConversionNode::timerCallback() {
+  // TODO
+}
+
+}  // namespace imu
+}  // namespace cg
