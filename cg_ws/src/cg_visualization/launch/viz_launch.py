1e8b7758c1580ca4c8b9e50d78d0004bf9b193c8
Russell Wong <russwong89@gmail.com>
2022-04-17 14:34:53 -0400

    Skeleton launch file for visualization

---
+++ cg_ws/src/cg_visualization/launch/viz_launch.py
@@ -0,0 +1,10 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+import yaml
+
+def generate_launch_description():
+  return LaunchDescription([
+    # TODO
+  ])
