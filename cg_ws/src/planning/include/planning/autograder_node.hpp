8b1322294118b0ee22c9eb6cfb149f97ce29129a
Alex Pletta <alexpletta@gmail.com>
2022-06-20 17:50:41 -0400

    remove slip estimate entirely from autograder, cli tested by Ben and Alex

--- cg_ws/src/planning/include/planning/autograder_node.hpp
+++ cg_ws/src/planning/include/planning/autograder_node.hpp
@@ -3,9 +3,7 @@
 #include <rclcpp/rclcpp.hpp>
 #include <cg_msgs/msg/mux_mode.hpp>
 #include <cg_msgs/msg/actuator_command.hpp>
-#include <cg_msgs/msg/slip.hpp>
 #include <cg_msgs/msg/encoder_telemetry.hpp>
-#include <sensor_msgs/msg/imu.hpp>
 
 namespace cg {
 namespace planning {
@@ -18,7 +16,6 @@ public:
 private: 
   /* Publishers and Subscribers */
   rclcpp::Publisher<cg_msgs::msg::ActuatorCommand>::SharedPtr cmd_pub_;
-  rclcpp::Subscription<cg_msgs::msg::Slip>::SharedPtr slip_sub_;
   rclcpp::Subscription<cg_msgs::msg::MuxMode>::SharedPtr mode_sub_; // Multiplexer mode number
 
   rclcpp::Subscription<cg_msgs::msg::EncoderTelemetry>::SharedPtr telem_sub_;
@@ -29,22 +26,17 @@ private:
   cg_msgs::msg::ActuatorCommand cmd_msg_;
 
   /* Callbacks */
-  void slipCallback(const cg_msgs::msg::Slip::SharedPtr msg); // read slip estimate
   void timerCallback(); // publish tool control commands
   void modeCallback(const cg_msgs::msg::MuxMode::SharedPtr msg);
   void telemCallback(const cg_msgs::msg::EncoderTelemetry::SharedPtr msg);
 
   /* Variables */
   uint8_t curr_mux_mode_; // Multiplexer operating mode
+  bool driving_forward_; 
 
-  /* Parameters */  
-  float slip_thresh_;
-  float half_deadband_;
+  /* Parameters */
   double design_blade_pos_;
   double max_des_blade_pos_;
-
-  bool driving_forward_;
-  bool slipping_;
 
 }; // class AutoGraderNode
 
