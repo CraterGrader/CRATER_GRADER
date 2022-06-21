5af0b2a3f39267bbce1e5141ef0dbc60687ebff7
Alex Pletta <alexpletta@gmail.com>
2022-06-20 20:11:00 -0400

    clean slip estimation files for only best attempt

--- cg_ws/src/localization/include/localization/slip_estimate_node.hpp
+++ cg_ws/src/localization/include/localization/slip_estimate_node.hpp
@@ -2,9 +2,7 @@
 
 #include<chrono>
 #include <rclcpp/rclcpp.hpp>
-#include <nav_msgs/msg/odometry.hpp>
 #include <cg_msgs/msg/slip.hpp>
-#include <cg_msgs/msg/encoder_telemetry.hpp>
 #include <cg_msgs/msg/actuator_state.hpp>
 #include <geometry_msgs/msg/pose_with_covariance_stamped.hpp>
 #include <cmath> // std::abs
@@ -23,52 +21,21 @@ public:
 private: 
   /* Publishers and Subscribers */
   rclcpp::Publisher<cg_msgs::msg::Slip>::SharedPtr slip_pub_;
-  // rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr enc_sub_;
-  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr global_sub_;
   rclcpp::Subscription<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr uwb_avg_sub_;
-
-  rclcpp::Subscription<cg_msgs::msg::EncoderTelemetry>::SharedPtr telem_sub_;
-
   rclcpp::Subscription<cg_msgs::msg::ActuatorState>::SharedPtr act_sub_;
 
   rclcpp::TimerBase::SharedPtr timer_; // For looping publish
 
   /* Message data */
   cg_msgs::msg::Slip slip_msg_;
-  float last_global_vel_;
-  float last_enc_vel_;
-  bool enc_init = false;
 
-  // Raw slip calculation
-  float actual_front_vel_;
-  float actual_rear_vel_;
-  float nonzero_slip_thresh_;
-  float curr_raw_slip_;
-
-  // Velocity estimate
-  bool global_init_ = true;
-  float curr_vel_estimate_;
-  float vel_twst_;
-  float last_x_;
-  float last_y_;
-  double last_t_;
-  double delta_t_;
-  int sampler_ = 0;
-  int sampling_steps_ = 12;
-
-  // Velocity estimate moving average filter
-  int vel_filter_window_ = 3;
-  std::list<float> vel_window_;
-  float curr_vel_avg_;
-  // float curr_vel_avg_;
-
-  /* Good params */
-  float vel_wheels_ = 0.0; // Wheel velocity averaged between front and back
+  /* Parameters */
+  float vel_wheels_ = 0.0; // Wheel velocity averaged between front and back, as reported from actuator state
   float curr_slip_ = 0.0;
   float nonzero_slip_thresh_wheel_ms_;
+  float nonzero_slip_thresh_vehicle_ms_;
   float slip_latch_thresh_ms_;
   float slip_velocity_latch_release_ms_;
-  float nonzero_slip_thresh_vehicle_ms_;
 
   int slip_window_size_ = 10;
   std::list<float> slip_window_;
@@ -78,14 +45,12 @@ private:
   std::list<float> vel_kf_window_;
   float vel_kf_avg_ = 0.0;
 
-  float slip_latch_thresh_ = 0.8;
-  float slip_velocity_latch_release_ = 2000;
   bool slip_latch_ = false;
 
   // Kalman filter for velocity estimation
   int kf_n_ = 4; // Number of states
   int kf_m_ = 2;            // Number of measurements
-  double kf_dt_ = 0.05; // Time step, should be ~hz of callback
+  double kf_dt_ = 0.05; // Time step in seconds: in theory, this should align with rate of prediction calls but might not in practice due to modeling error
   float vel_kf_ = 0.;       // Estimated velocity from Kalman Filter
 
   float Qx_;
@@ -106,23 +71,13 @@ private:
   cg::localization::KalmanFilterLinear kf_vel_;
 
   /* Callbacks */
-  void slipCallback();
-  void globalCallback(const nav_msgs::msg::Odometry::SharedPtr global_msg);
-  void encCallback(const nav_msgs::msg::Odometry::SharedPtr enc_msg);
   void actStateCallback(const cg_msgs::msg::ActuatorState::SharedPtr msg);
-  void slipTelemetryCallback(const cg_msgs::msg::EncoderTelemetry::SharedPtr msg);
   void timerCallback(); // For looping publish
   void uwbAvgCallback(const geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr msg);
 
-  // rclcpp::TimerBase::SharedPtr timer_ = this->create_wall_timer(
-  //   std::chrono::milliseconds(50),
-  //   std::bind(&SlipEstimateNode::slipCallback, this));
-
   /* Helpers */
-  void updateMovingAverage();
-  float updateMovingAverage(std::list<float> &list, float &new_val, int window_size);
-
-  // Syncronize messages before processing
+  // Update list of values for moving average, modify the list being passed as an argument
+  float updateMovingAverage(std::list<float> &list, const float &new_val, const int &window_size);
 
 }; // class SlipEstimateNode
 
--- cg_ws/src/localization/src/slip_estimate_node.cpp
+++ cg_ws/src/localization/src/slip_estimate_node.cpp
@@ -1,5 +1,4 @@
 #include "localization/slip_estimate_node.hpp"
-#include <math.h>
 
 namespace cg {
 namespace slip {
@@ -10,21 +9,9 @@ SlipEstimateNode::SlipEstimateNode() : Node("slip_estimate_node") {
   slip_pub_ = this->create_publisher<cg_msgs::msg::Slip>(
     "/slip_estimate", 1
   );
-  
-  // enc_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
-  //   "/encoder/odom", 1, std::bind(&SlipEstimateNode::encCallback, this, std::placeholders::_1)
-  // );
-
-  global_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
-    "/odometry/filtered/ekf_global_node", 1, std::bind(&SlipEstimateNode::globalCallback, this, std::placeholders::_1)
-  );
 
   uwb_avg_sub_ = this->create_subscription<geometry_msgs::msg::PoseWithCovarianceStamped>(
       "/uwb_beacon/average_tag", 1, std::bind(&SlipEstimateNode::uwbAvgCallback, this, std::placeholders::_1));
-
-  telem_sub_ = this->create_subscription<cg_msgs::msg::EncoderTelemetry>(
-    "/encoder_telemetry", 1, std::bind(&SlipEstimateNode::slipTelemetryCallback, this, std::placeholders::_1)
-  );
 
   act_sub_ = this->create_subscription<cg_msgs::msg::ActuatorState>(
       "/actuator/state", 1, std::bind(&SlipEstimateNode::actStateCallback, this, std::placeholders::_1));
@@ -35,19 +22,6 @@ SlipEstimateNode::SlipEstimateNode() : Node("slip_estimate_node") {
       std::bind(&SlipEstimateNode::timerCallback, this));
 
   // Load parameters
-  this->declare_parameter<float>("nonzero_slip_thresh", 10.0);
-  this->get_parameter("nonzero_slip_thresh", nonzero_slip_thresh_);
-  this->declare_parameter<float>("nonzero_slip_thresh_wheel_ms", 0.1);
-  this->get_parameter("nonzero_slip_thresh_wheel_ms", nonzero_slip_thresh_wheel_ms_);
-  this->declare_parameter<float>("nonzero_slip_thresh_vehicle_ms_", 0.07);
-  this->get_parameter("nonzero_slip_thresh_vehicle_ms_", nonzero_slip_thresh_vehicle_ms_);
-  this->declare_parameter<float>("slip_latch_thresh_ms", 0.8);
-  this->get_parameter("slip_latch_thresh_ms", slip_latch_thresh_ms_);
-  this->declare_parameter<float>("slip_velocity_latch_release_ms", 0.06);
-  this->get_parameter("slip_velocity_latch_release_ms", slip_velocity_latch_release_ms_);
-  this->declare_parameter<int>("slip_window_size", 10);
-  this->get_parameter("slip_window_size", slip_window_size_);
-
   this->declare_parameter<float>("Qx", 10.0);
   this->get_parameter("Qx", Qx_);
   this->declare_parameter<float>("Qxdot", 10.0);
@@ -73,6 +47,19 @@ SlipEstimateNode::SlipEstimateNode() : Node("slip_estimate_node") {
 
   this->declare_parameter<int>("vel_kf_window_size", 10);
   this->get_parameter("vel_kf_window_size", vel_kf_window_size_);
+
+  this->declare_parameter<float>("nonzero_slip_thresh_wheel_ms", 0.1);
+  this->get_parameter("nonzero_slip_thresh_wheel_ms", nonzero_slip_thresh_wheel_ms_);
+  this->declare_parameter<float>("nonzero_slip_thresh_vehicle_ms_", 0.07);
+  this->get_parameter("nonzero_slip_thresh_vehicle_ms_", nonzero_slip_thresh_vehicle_ms_);
+
+  this->declare_parameter<float>("slip_latch_thresh_ms", 0.8);
+  this->get_parameter("slip_latch_thresh_ms", slip_latch_thresh_ms_);
+  this->declare_parameter<float>("slip_velocity_latch_release_ms", 0.06);
+  this->get_parameter("slip_velocity_latch_release_ms", slip_velocity_latch_release_ms_);
+
+  this->declare_parameter<int>("slip_window_size", 10);
+  this->get_parameter("slip_window_size", slip_window_size_);
 
   // Kalman Filter for velocity estimation
   // Discrete LTI projectile motion, measuring position only, constant velocity kinematics
@@ -105,10 +92,9 @@ SlipEstimateNode::SlipEstimateNode() : Node("slip_estimate_node") {
         .0, 0, Py_, .0,
         .0, 0, .0, Pydot_;
 
-  // Construct the filter
+  // Construct the filter using initializer list with temp object
   cg::localization::KalmanFilterLinear kf_vel_tmp_(A_, H_, Q_, R_, P_);
-
-  kf_vel_ = kf_vel_tmp_;
+  kf_vel_ = kf_vel_tmp_; // Assign temp object to the actual object that is declared in node header
 
   // Best guess of initial states
   x0_ << 0, 0, 0, 0; // [x; xdot; y; ydot]
@@ -149,15 +135,11 @@ void SlipEstimateNode::timerCallback()
     }
   }
 
-  // Not used
-  slip_msg_.vel_int = curr_vel_estimate_;
-  slip_msg_.vel_twst = vel_twst_;
-
-  // Used
+  // Publish the message; some extra values included to assist testing
   slip_msg_.slip = curr_slip_;
   slip_msg_.slip_avg = slip_avg_;
   slip_msg_.slip_latch = slip_latch_;
-  slip_msg_.vel_kf = vel_kf_;
+  slip_msg_.vel = vel_kf_;
   slip_msg_.vel_avg = vel_kf_avg_;
   slip_msg_.header.stamp = this->get_clock()->now();
   slip_pub_->publish(slip_msg_);
@@ -175,81 +157,20 @@ void SlipEstimateNode::uwbAvgCallback(const geometry_msgs::msg::PoseWithCovarian
   Eigen::VectorXd z_(kf_m_);
   z_ << msg->pose.pose.position.x, msg->pose.pose.position.y;
 
+  // Cycle estimator
   kf_vel_.predict();
   kf_vel_.update(z_);
+
+  // Extract estimate of velocity magnitude
   Eigen::VectorXd xhat_(kf_n_);
-  xhat_ = kf_vel_.state();                                 // [x; xdot; y; ydot]
-  vel_kf_ = sqrt(pow(xhat_(1), 2.0) + pow(xhat_(3), 2.0)); // / 0.00008298755187;
+  xhat_ = kf_vel_.state(); // [x; xdot; y; ydot]
+  vel_kf_ = sqrt(pow(xhat_(1), 2.0) + pow(xhat_(3), 2.0));
 
   // Update moving average
   vel_kf_avg_ = updateMovingAverage(vel_kf_window_, vel_kf_, vel_kf_window_size_);
 }
 
-void SlipEstimateNode::slipTelemetryCallback(const cg_msgs::msg::EncoderTelemetry::SharedPtr msg) {
-
-  // Calculate current raw slip value
-  actual_front_vel_ = static_cast < float > (std::abs(msg->drive_vel_rear));
-  actual_rear_vel_ = static_cast<float>(std::abs(msg->drive_vel_front)); // TODO: get value from config file as message constant
-
-  // if (actual_rear_vel_ > nonzero_slip_thresh_) 
-  // {
-  //   // Calculate slip if it's above the calculation threshold
-  //   // Expect 0 for no slip, 1 for 100% slip, clamp at zero
-  //   curr_raw_slip_ = std::max(static_cast<float>(0.0), (actual_rear_vel_ - actual_front_vel_) / actual_rear_vel_);
-  // }
-  // else {
-  //   curr_raw_slip_ = static_cast<float>(0.0);
-  // }
-
-  // // Filter slip estimate with moving average
-  // updateMovingAverage(); // call to update curr_slip_avg_
-
-  // // Publish the slip estimate
-  // slip_msg_.slip = curr_slip_avg_;
-  // slip_msg_.header.stamp = this->get_clock()->now();
-  // slip_pub_->publish(slip_msg_);
-
-  // Slip is percentage difference between encoder-read velocty and fused odometry velocity.
-}
-
-
-// void SlipEstimateNode::slipCallback() {
-
-//   // If either are not initialized, can't calculate slip
-//   if (!global_init || !enc_init) return;
-
-//   slip_msg_.header.stamp = this->get_clock()->now();
-
-//   if (last_enc_vel_ != 0.0) 
-//   {
-//     slip_msg_.slip = 100 * (last_enc_vel_ - last_global_vel_) / last_enc_vel_;
-//   }
-//   else {
-//     slip_msg_.slip = 0;
-//   }
-
-//   slip_pub_->publish(slip_msg_);
-
-//   // Slip is percentage difference between encoder-read velocty and fused odometry velocity.
-
-
-// }
-
-// void SlipEstimateNode::updateVelMovingAverage()
-// {
-
-//   // Build up the filter
-//   vel_window_.push_front(curr_vel_estimate_);
-//   if (static_cast<int>(vel_window_.size()) > vel_filter_window_)
-//   {
-//     vel_window_.pop_back(); // remove oldest value
-//   }
-
-//   // Update the filter
-//   curr_vel_avg_ = std::accumulate(vel_window_.begin(), vel_window_.end(), 0.0) / vel_window_.size();
-// }
-
-float SlipEstimateNode::updateMovingAverage(std::list<float> &list, float &new_val, int window_size)
+float SlipEstimateNode::updateMovingAverage(std::list<float> &list, const float &new_val, const int &window_size)
 {
   // Build up the filter
   list.push_front(new_val);
@@ -261,100 +182,6 @@ float SlipEstimateNode::updateMovingAverage(std::list<float> &list, float &new_v
   // Update the filter
   return std::accumulate(list.begin(), list.end(), 0.0) / list.size();
 }
-
-void SlipEstimateNode::globalCallback(const nav_msgs::msg::Odometry::SharedPtr msg) {
-
-  // // Kalman filter velocity estimate
-  // Eigen::VectorXd z_(kf_m_);
-  // z_ << msg->pose.pose.position.x, msg->pose.pose.position.y;
-
-  // kf_vel_.predict();
-  // kf_vel_.update(z_);
-  // Eigen::VectorXd xhat_(kf_n_);
-  // xhat_ = kf_vel_.state();                                 // [x; xdot; y; ydot]
-  // vel_kf_ = sqrt(pow(xhat_(1), 2.0) + pow(xhat_(3), 2.0)); // / 0.00008298755187;
-
-  // vel_kf_avg_ = updateMovingAverage(vel_kf_window_, vel_kf_, vel_kf_window_size_);
-
-  // Initialize last time step
-  if (global_init_)
-  {
-    last_x_ = msg->pose.pose.position.x;
-    last_y_ = msg->pose.pose.position.y;
-    last_t_ = msg->header.stamp.sec + msg->header.stamp.nanosec * 1e-9;
-  }
-  global_init_ = false;
-
-  // Only sample every n steps
-  // if (sampler_++ % sampling_steps_ != 0) {
-  //   return;
-  // }
-
-  // Change in time
-  delta_t_ = (msg->header.stamp.sec + msg->header.stamp.nanosec * 1e-9) - last_t_;
-
-  // Make velocity estimate
-  curr_vel_estimate_ = sqrt(
-      pow(msg->pose.pose.position.x - last_x_, 2.0) +
-      pow(msg->pose.pose.position.y - last_y_, 2.0)) / delta_t_;
-
-  curr_vel_estimate_ = curr_vel_estimate_ / 0.00008298755187;
-
-  vel_twst_ = sqrt(pow(msg->twist.twist.linear.x, 2.0) + pow(msg->twist.twist.linear.y, 2.0));
-
-  vel_twst_ = vel_twst_ / 0.00008298755187;
-
-  // Filter slip estimate with moving average
-  curr_vel_avg_ = updateMovingAverage(vel_window_, curr_vel_estimate_, vel_filter_window_); // call to update filtered slip estimate
-
-  // Update last time step
-  last_x_ = msg->pose.pose.position.x;
-  last_y_ = msg->pose.pose.position.y;
-  last_t_ = msg->header.stamp.sec + msg->header.stamp.nanosec * 1e-9;
-
-  // if (actual_rear_vel_ > nonzero_slip_thresh_)
-  // {
-  //   // Calculate slip if it's above the calculation threshold
-  //   // Expect 0 for no slip, 1 for 100% slip, clamp at zero
-  //   curr_raw_slip_ = std::max(static_cast<float>(0.0), (actual_rear_vel_ - curr_vel_avg_) / actual_rear_vel_);
-  // }
-  // else
-  // {
-  //   curr_raw_slip_ = static_cast<float>(0.0);
-  // }
-
-  // // Evaluate slip latch conditions
-  // if (!slip_latch_) {
-  //   if (curr_raw_slip_ > slip_latch_thresh_) 
-  //   {
-  //     slip_latch_ = true;
-  //   }
-  // }
-  // else {
-  //   if (curr_vel_avg_ > slip_velocity_latch_release_)
-  //   {
-  //     slip_latch_ = false;
-  //   }
-  // }
-
-  // Publish the slip estimate
-  // slip_msg_.slip_latch = slip_latch_;
-  // slip_msg_.vel_kf = vel_kf_;
-  // slip_msg_.header.stamp = this->get_clock()->now();
-  // slip_pub_->publish(slip_msg_);
-}
-
-
-// void SlipEstimateNode::encCallback(
-//   const nav_msgs::msg::Odometry::SharedPtr enc_msg) {
-
-//   last_enc_vel_ = sqrt(
-//     pow(enc_msg->twist.twist.linear.x, 2.0) + 
-//     pow(enc_msg->twist.twist.linear.y, 2.0));
-
-//   enc_init = true;
-
-// }
 
 } // namespace odom
 } // namespace cg
