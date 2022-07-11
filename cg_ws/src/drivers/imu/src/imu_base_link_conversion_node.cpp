6171200bbe8c3a9e7185aff7ca8ad513eb49f429
Russell Wong <russwong89@gmail.com>
2022-07-10 21:57:41 -0700

    Deleted commented code in imu_baselink_conversion_node

--- cg_ws/src/drivers/imu/src/imu_base_link_conversion_node.cpp
+++ cg_ws/src/drivers/imu/src/imu_base_link_conversion_node.cpp
@@ -81,7 +81,6 @@ void ImuBaseLinkConversionNode::doTransform(
   imu_out.angular_velocity.z = vel.z();
 
   imu_out.angular_velocity_covariance = imu_in.angular_velocity_covariance;
-  // transformCovariance(imu_in.angular_velocity_covariance, imu_out.angular_velocity_covariance, r);
 
   Eigen::Vector3d acc = t * Eigen::Vector3d(
     imu_in.linear_acceleration.x,
@@ -93,7 +92,6 @@ void ImuBaseLinkConversionNode::doTransform(
   imu_out.linear_acceleration.z = acc.z();
 
   imu_out.linear_acceleration_covariance = imu_in.linear_acceleration_covariance;
-  // transformCovariance(imu_in.linear_acceleration_covariance, imu_out.linear_acceleration_covariance, r);
 
 
   Eigen::Quaternion<double> orientation = r * Eigen::Quaternion<double>(
@@ -109,7 +107,6 @@ void ImuBaseLinkConversionNode::doTransform(
   imu_out.orientation.z = orientation.z();
 
   imu_out.orientation_covariance = imu_in.orientation_covariance;
-  // transformCovariance(imu_in.orientation_covariance, imu_out.orientation_covariance, r);
 }
 
 void ImuBaseLinkConversionNode::transformCovariance(
