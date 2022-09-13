afdf998c7f7ecf9f82e4a09d9ef6ec586ecd1a41
apletta <alexpletta@gmail.com>
2022-09-13 12:53:16 -0400

    Revert "make the code do the svd (#26)"
    This reverts commit e317c29ad04c786c1bfe5c61fa14d3db6f151f54.

--- cg_ws/src/drivers/imu/src/vn_imu_node.cpp
+++ cg_ws/src/drivers/imu/src/vn_imu_node.cpp
@@ -40,7 +40,7 @@ VnImuNode::VnImuNode() : Node("vn_imu_node") {
 
   // Initialize publishers and subscribers
   imu_pub_ = this->create_publisher<sensor_msgs::msg::Imu>(
-    "/imu/data/imu_link", 1
+    "/imu/data", 1
   );
   timer_ = this->create_wall_timer(
     std::chrono::milliseconds(1000/freq),
@@ -61,8 +61,7 @@ void VnImuNode::timerCallback() {
   q.setRPY(
     M_PI/180.0*vs_data_register.yawPitchRoll[2] - orientation_zero_offsets_.x,
     M_PI/180.0*vs_data_register.yawPitchRoll[1] - orientation_zero_offsets_.y,
-    // 0.0  // Intentionally ignore yaw assuming no magnetometer data on the moon, and set to zero
-    M_PI/180.0*vs_data_register.yawPitchRoll[0] - orientation_zero_offsets_.z
+    0.0  // Intentionally ignore yaw assuming no magnetometer data on the moon, and set to zero
   );
   q.normalize();
   msg.orientation = tf2::toMsg(q);
