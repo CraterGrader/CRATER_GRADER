a6e35702ddeaaab34bf6f018efb5e402a9e056df
Russell Wong <russwong89@gmail.com>
2022-09-27 17:44:28 -0400

    Added transport planner skeleton

---
+++ cg_ws/src/planning/test/transport_planner_test.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include <planning/transport_planner.hpp>
+
+// TEST(TransportPlannerTest, helloworld)
+// {
+//   float expected = 0.0f;
+//   float actual = 0.0f;
+//   float absolute_range = 0.0f;
+//   EXPECT_NEAR(expected, actual, absolute_range);
+// }
