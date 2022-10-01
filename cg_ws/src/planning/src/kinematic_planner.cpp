e4b96ebb3d7aa0dddee4772bc544e6bf85b1fae7
John Harrington <harrington.john@husky.neu.edu>
2022-10-01 14:15:40 -0400

    Added lattice generator

--- cg_ws/src/planning/src/kinematic_planner.cpp
+++ cg_ws/src/planning/src/kinematic_planner.cpp
@@ -30,43 +30,76 @@ namespace planning {
 
         std::vector<std::vector<cg_msgs::msg::Pose2D>> lattice;
         for (float turn_radius: turn_radii) {
-            lattice.push_back(generateLatticeArm(turn_radius));
+            lattice.push_back(generateLatticeArm(turn_radius, true, true));
+            lattice.push_back(generateLatticeArm(turn_radius, true, false));
             
             // Account for "negative" turn radius
             if (abs(turn_radius) > 1e-2) {
-                lattice.push_back(generateLatticeArm(-turn_radius));
+                lattice.push_back(generateLatticeArm(turn_radius, false, true));
+                lattice.push_back(generateLatticeArm(turn_radius, false, false));
             }
         }
 
         return lattice;
     }
 
-    std::vector<cg_msgs::msg::Pose2D> KinematicPlanner::generateLatticeArm(float turn_radius) {
+    std::vector<cg_msgs::msg::Pose2D> KinematicPlanner::generateLatticeArm(float turn_radius, bool forwards, bool right) {
+
+        // Since left/right is handled with flags, turn radius should always be positive
+        assert(turn_radius > 0);
 
         std::vector<cg_msgs::msg::Pose2D> lattice_arm;
-
         int num_segments = ceil(max_trajectory_length / trajectory_resolution);
+        float x,y,yaw;
+        x = y = yaw 0.0;
 
-        // Forward cases
+        float trajectory_arc = max_trajectory_length;
+        if (!forwards) max_trajectory_length *= -1;
 
-        // Straight case
-        if (turn_radius < 1e-2) {
+        // Straight (forwards and backwards)
+        if (abs(turn_radius) < 1e-2) {
+            float incremental_movement = static_cast<float>(max_trajectory_length / num_segments);
+            if (!forwards) incremental_movement *= -1;
 
-            
-
-
+            for (int n = 0; n <= num_segments; ++n) {
+                x += incremental_movement;
+                cg_msgs::msg::Pose2D pose = create_pose2d(x, y, yaw);
+                lattice_arm.push_back(pose);
+            }
         }
+        // Right (forwards and backwards)
+        else if (right) {
+            float start_rad = M_PI/2;
+            float stop_rad = (-max_trajectory_length / turn_radius) + start_rad;
+            float rad_increment = (stop_rad - start_rad) / num_segments;
 
+            float cur_rad = start_rad;
+            for (int n = 0; n <= num_segments; ++n) {
+                x = turn_radius * cos(cur_rad);
+                y = turn_radius * sin(cur_rad) - turn_radius;
+                yaw = M_PI + atan2(-cos(cur_rad), sin(cur_rad));
 
-        // Backwards cases 
+                cg_msgs::msg::Pose2D pose = create_pose2d(x, y, yaw);
+                lattice_arm.push_back(pose);
+                cur_rad += rad_increment;
+            }
+        // Left (forwards & backwards)
+        } else {
+            float start_rad = -M_PI/2;
+            float stop_rad = (max_trajectory_length / turn_radius) + start_rad;
+            float rad_increment = (stop_rad - start_rad) / num_segments;
 
-        // Straight case
-        if (turn_radius < 1e-2) {
+            float cur_rad = start_rad;
+            for (int n = 0; n <= num_segments; ++n) {
+                x = turn_radius * cos(cur_rad);
+                y = turn_radius * sin(cur_rad) + turn_radius;
+                yaw = atan2(-cos(cur_rad), sin(cur_rad));
 
-
+                cg_msgs::msg::Pose2D pose = create_pose2d(x, y, yaw);
+                lattice_arm.push_back(pose);
+                cur_rad += rad_increment;
+            }
         }
-
-
 
         return lattice_arm;
     }
