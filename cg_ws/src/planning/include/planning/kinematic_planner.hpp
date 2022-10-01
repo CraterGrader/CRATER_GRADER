c1e3d57d806c59077c3314fc3adbcfc1c473472c
John Harrington <harrington.john@husky.neu.edu>
2022-10-01 10:27:37 -0400

    Added header definitions for most required functions

--- cg_ws/src/planning/include/planning/kinematic_planner.hpp
+++ cg_ws/src/planning/include/planning/kinematic_planner.hpp
@@ -18,6 +18,49 @@ public:
     std::vector<cg_msgs::msg::Pose2D> &path,
     const cg_msgs::msg::Pose2D &agent_pose,
     const cg::mapping::SiteMap &map);
+
+  // Truncates trajectory to closest pose to goal_pose
+  cg_msgs::msg::Pose2D getClosestTrajectoryPoseToGoal(
+    const std::vector<cg_msgs::msg::Pose2D> &trajectory, 
+    const cg_msgs::msg::Pose2D &goal_pose);
+
+  // Checks if trajectory_end_pose is within distance threshold of goal_pose
+  bool samePoseWithThresh(
+    const cg_msgs::msg::Pose2D &trajectory_end_pose,
+    const cg_msgs::msg::Pose2D &goal_pose);
+
+  // Creates a std::vector of lattice trajectories 
+  // based on a base lattice type and current pose
+  std::vector<std::vector<cg_msgs::msg::Pose2D>> transformLatticeToPose(
+    const std::vector<cg_msgs::msg::Pose2D> &base_lattice,
+    const cg_msgs::msg::Pose2D &current_pose);
+
+  // Checks if trajectory is valid (eg. within worksite)
+  bool isValidTrajectory(
+    const std::vector<cg_msgs::msg::Pose2D> &trajectory);
+
+  // Calculates the total cost of the trajectory
+  std::vector<float> calculateTrajectoryCost(
+    const std::vector<cg_msgs::msg::Pose2D> &trajectory,
+    const cg::mapping::SiteMap &map);
+
+  // Calculate heuristic associated with trajectory
+  float trajectory_heuristic(
+    const std::vector<cg_msgs::msg::Pose2D> &trajectory, 
+    const cg_msgs::msg::Pose2D &goal_pose);
+
+  // Threshold to determine if trajectory end pose is a valid final pose
+  float goal_pose_distance_threshold;
+
+  // Lattice Parameters
+  std::vector<float> turn_radii;
+  float max_trajectory_length;
+  float trajectory_resolution;
+
+  // Cost Parameters
+  float topography_weight;
+  float trajectory_heuristic_epsilon;
+
 };
 
 } // namespace planning
