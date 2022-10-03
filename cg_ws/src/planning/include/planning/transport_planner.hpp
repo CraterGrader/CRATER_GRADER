e734e8c84034d727c966ffc3064185d90b92841f
Alex Pletta <alexpletta@gmail.com>
2022-10-02 23:16:28 -0400

    added integration support for ortools

--- cg_ws/src/planning/include/planning/transport_planner.hpp
+++ cg_ws/src/planning/include/planning/transport_planner.hpp
@@ -4,16 +4,17 @@
 #include <planning/common.hpp>
 #include <planning/goal_planner.hpp>
 #include <mapping/site_map.hpp>
-
+#include "ortools/linear_solver/linear_solver.h"
 namespace cg {
 namespace planning {
 
 class TransportPlanner : public GoalPlanner {
 
 public:
+  TransportPlanner() {};
   cg_msgs::msg::Pose2D getGoalPose(
     const cg_msgs::msg::Pose2D &agent_pose, const cg::mapping::SiteMap &map);
-
+  float basicExample(); // Example from ortools, should always return optimal value of 4: https://developers.google.com/optimization/introduction/cpp#complete-program
 private: 
   // tuple<std::vector<cg::mapping::indexPoint>, std::vector<cg::mapping::indexPoint> > = gen_nodes_from_height_map(std::vector<float> map);
  
--- cg_ws/src/planning/test/transport_planner_test.cpp
+++ cg_ws/src/planning/test/transport_planner_test.cpp
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 #include <planning/transport_planner.hpp>
-#include <planning/ceres_helloworld.hpp>
+// #include <planning/ceres_helloworld.hpp>
 
 TEST(TransportPlannerTest, helloworld){
   float expected = 0.0f;
@@ -36,3 +36,15 @@ TEST(TransportPlannerTest, ceres_helloworld){
   EXPECT_NEAR(expected, actual, absolute_range);
 
 }
+
+TEST(TransportPlannerTest, ortools_helloworld){
+  cg::planning::TransportPlanner transport_planner;
+  // Example from ortools, should always return optimal value of 4: https://developers.google.com/optimization/introduction/cpp#complete-program
+  float val = transport_planner.basicExample();
+
+  float expected = 4.0f;
+  float actual = val;
+  EXPECT_EQ(expected, actual);
+}
+
+// colcon test --packages-select planning --event-handlers console_cohesion+
