89ffd99d56430c394a8f77ffd864e5d4f9289831
Russell Wong <russwong89@gmail.com>
2022-10-02 22:45:27 -0400

    Add class fields and constructor

--- cg_ws/src/planning/include/planning/exploration_planner.hpp
+++ cg_ws/src/planning/include/planning/exploration_planner.hpp
@@ -11,11 +11,16 @@ namespace planning {
 class ExplorationPlanner : public GoalPlanner {
 
 public:
+  ExplorationPlanner() : curr_waypoint_idx_(0) {};
   cg_msgs::msg::Pose2D getGoalPose(
     const cg_msgs::msg::Pose2D &agent_pose, const cg::mapping::SiteMap &map);
-  void planExploration(
+  //
+  bool planExploration(
     const cg_msgs::msg::Pose2D &agent_pose, const cg::mapping::SiteMap &map);
-  
+
+private:
+  std::vector<cg_msgs::msg::Pose2D> exploration_waypoints_;
+  int curr_waypoint_idx_;
 };
 
 } // namespace planning
