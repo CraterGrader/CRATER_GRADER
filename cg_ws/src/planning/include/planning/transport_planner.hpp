cf720058245211cf0487ac2388d1c3eb9757af75
Ben Younes <byounes@andrew.cmu.edu>
2022-10-07 10:13:05 -0400

    untested transport planner

--- cg_ws/src/planning/include/planning/transport_planner.hpp
+++ cg_ws/src/planning/include/planning/transport_planner.hpp
@@ -8,6 +8,13 @@
 namespace cg {
 namespace planning {
 
+struct TransportNode {
+  // a struct which simply defines a points position in continous 3-space
+    float x;
+    float y;
+    float height;
+};
+
 class TransportPlanner : public GoalPlanner {
 
 public:
@@ -15,18 +22,13 @@ public:
   cg_msgs::msg::Pose2D getGoalPose(
     const cg_msgs::msg::Pose2D &agent_pose, const cg::mapping::SiteMap &map);
   float basicExample(); // Example from ortools, should always return optimal value of 4: https://developers.google.com/optimization/introduction/cpp#complete-program
+
+  float solveEMDtoy(); // prototype of EMD solve
+  float solveEMDhardMap(); // EMD Solve hard coded map
+
 private: 
-  // tuple<std::vector<cg::mapping::indexPoint>, std::vector<cg::mapping::indexPoint> > = gen_nodes_from_height_map(std::vector<float> map);
- 
-  // distance_matrix  setup_distance_matrix(src_nodes, sink_nodes)
- 
-  // src_array, sink_array = setup_height_arrays(src_nodes, sink_nodes)
- 
-  // policy solve_emd(sink_array, src_array, distance_matrix)
- 
-  // transport_list = get_transport_list(policy, src_nodes, sink_nodes)
- 
-  // nearest_transport, transport_idx = find_nearest_transport(transport_list, agent.pose, visited_transport_indices)
+
+  
 
 };
 
--- cg_ws/src/planning/test/transport_planner_test.cpp
+++ cg_ws/src/planning/test/transport_planner_test.cpp
@@ -9,34 +9,6 @@ TEST(TransportPlannerTest, helloworld){
   EXPECT_NEAR(expected, actual, absolute_range);
 }
 
-TEST(TransportPlannerTest, ceres_helloworld){
-    //   google::InitGoogleLogging(argv[0]);
-    //   // The variable to solve for with its initial value. It will be
-    //   // mutated in place by the solver.
-    //   double x = 0.5;
-    //   const double initial_x = x;
-    //   // Build the problem.
-    //   Problem problem;
-    //   // Set up the only cost function (also known as residual). This uses
-    //   // auto-differentiation to obtain the derivative (jacobian).
-    //   CostFunction* cost_function =
-    //       new AutoDiffCostFunction<CostFunctor, 1, 1>(new CostFunctor);
-    //   problem.AddResidualBlock(cost_function, nullptr, &x);
-    //   // Run the solver!
-    //   Solver::Options options;
-    //   options.minimizer_progress_to_stdout = true;
-    //   Solver::Summary summary;
-    //   Solve(options, &problem, &summary);
-    //   std::cout << summary.BriefReport() << "\n";
-    //   std::cout << "x : " << initial_x << " -> " << x << "\n";
-
-  float expected = 0.0f;
-  float actual = 0.0f;
-  float absolute_range = 0.0f;
-  EXPECT_NEAR(expected, actual, absolute_range);
-
-}
-
 TEST(TransportPlannerTest, ortools_helloworld){
   cg::planning::TransportPlanner transport_planner;
   // Example from ortools, should always return optimal value of 4: https://developers.google.com/optimization/introduction/cpp#complete-program
@@ -45,6 +17,16 @@ TEST(TransportPlannerTest, ortools_helloworld){
   float expected = 4.0f;
   float actual = val;
   EXPECT_EQ(expected, actual);
+}
+
+TEST(TransportPlannerTest, ortools_EMD_baby){
+  cg::planning::TransportPlanner transport_planner;
+  // Example from ortools, should always return optimal value of 4: https://developers.google.com/optimization/introduction/cpp#complete-program
+  float val = transport_planner.solveEMDtoy();
+
+  float expected = 1.8f;
+  float actual = val;
+  EXPECT_NEAR(expected, actual,10.0);
 }
 
 // colcon test --packages-select planning --event-handlers console_cohesion+
