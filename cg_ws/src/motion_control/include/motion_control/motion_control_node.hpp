55e5580f406d3ae77bf7987111df9c87fd1a6d6d
Russell Wong <russwong89@gmail.com>
2022-10-13 19:30:14 -0400

    PIDController skeleton

--- cg_ws/src/motion_control/include/motion_control/motion_control_node.hpp
+++ cg_ws/src/motion_control/include/motion_control/motion_control_node.hpp
@@ -3,6 +3,7 @@
 #include <rclcpp/rclcpp.hpp>
 #include <cg_msgs/msg/actuator_command.hpp>
 #include <cg_msgs/msg/trajectory.hpp>
+#include "motion_control/pid_controller.hpp"
 
 namespace cg {
 namespace motion_control {
@@ -17,11 +18,11 @@ private:
   rclcpp::Publisher<cg_msgs::msg::ActuatorCommand>::SharedPtr cmd_pub_;
   rclcpp::Subscription<cg_msgs::msg::Trajectory>::SharedPtr trajectory_sub_;
 
-  /* Message data */
-  // nav_msgs::msg::Odometry odom_msg_;
-
   /* Callbacks */
   void trajectoryCallback(const cg_msgs::msg::Trajectory::SharedPtr msg);
+
+  /* Controllers */
+  PIDController longitudinal_controller_;
 
   /* Parameters */  
   double longitudinal_velocity_kp_;
---
+++ cg_ws/src/motion_control/src/pid_controller.cpp
@@ -0,0 +1,11 @@
+#include "motion_control/pid_controller.hpp"
+
+namespace cg {
+namespace motion_control {
+
+PIDController::PIDController(const PIDParams &params) {
+  // TODO
+}
+
+}  // namespace motion_control
+}  // namespace cg
