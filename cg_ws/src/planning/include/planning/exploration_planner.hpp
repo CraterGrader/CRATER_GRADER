158d8b07c50e91a853bcc5f653fdc5755488c963
ben younes <77645213+byounes100@users.noreply.github.com>
2022-10-13 10:53:39 -0400

    Cg 429 exploration planner (#59)
    * Add class fields and constructor
    
    * Implement getGoalPose and isGoalReached
    
    * Implement planExploration
    
    * Change SiteMap to Map<float> in exploration planner and goal planner
    
    * Return list of waypoints in planExploration
    
    * Add unit tests
    
    * Add test for isExplorationDone
    
    * added utility for vector return of getGoalPose
    
    Co-authored-by: Russell Wong <russwong89@gmail.com>

--- cg_ws/src/planning/include/planning/exploration_planner.hpp
+++ cg_ws/src/planning/include/planning/exploration_planner.hpp
@@ -3,7 +3,7 @@
 
 #include <planning/common.hpp>
 #include <planning/goal_planner.hpp>
-#include <mapping/site_map.hpp>
+#include <mapping/map.hpp>
 
 namespace cg {
 namespace planning {
@@ -11,11 +11,13 @@ namespace planning {
 class ExplorationPlanner : public GoalPlanner {
 
 public:
+  ExplorationPlanner();
+ 
   std::vector<cg_msgs::msg::Pose2D> getGoalPose(
     const cg_msgs::msg::Pose2D &agent_pose, const cg::mapping::Map<float> &map);
-  void planExploration(
-    const cg_msgs::msg::Pose2D &agent_pose, const cg::mapping::SiteMap &map);
-  
+
+private:
+  static constexpr double min_dist_from_map_boundary_ = 0.5;
 };
 
 } // namespace planning
