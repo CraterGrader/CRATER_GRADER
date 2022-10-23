b21dd635b410a7d0f3e03d0583fcec1ea1b5f769
ben younes <77645213+byounes100@users.noreply.github.com>
2022-10-23 19:25:04 -0400

    Cg 467 yaw improvements (#74)
    * Recalibrated to 1080p, added moving median-mean filter, working STags
    
    * Updated exact rotation of tag based on FARO scan
    
    * Updated to have correct positioning from STags using Pitch
    
    * 1080x1920 for AprilTag yaw
    
    * Set back to onboard 1080p
    
    * Added video2
    
    * video2
    
    * Revert to suncam
    
    * Updated to cg_dev for camera input
    
    * Switched to 720p
    
    * switch to 1920
    
    * fix syntax
    
    * Updated to correct 1080:
    
    * Try offboard calibration
    
    * Back to 720
    
    * Used scaled 720p
    
    * Back to calibrated 1080
    
    * Removed scaled 1080
    
    * back to onboard 1080
    
    * Back to 720
    
    * Backt o 1080
    
    * 720
    
    * Yaw Update
    
    * yaw rviz config file
    
    * Yaw Changes
    
    * yaw
    
    * Improved the yaw with camera-correction + pose yaw estimation
    
    * Removed images
    
    * Update cgdev.dockerfile
    
    Co-authored-by: ryan1288 <ryanl1288@hotmail.com>
    Co-authored-by: Ryan Lee <43348991+ryan1288@users.noreply.github.com>
    Co-authored-by: CraterGrader Bot <cratergrader@protonmail.com>
    Co-authored-by: CraterGrader <cratergrader@email.com>

---
+++ tools/camera_calib_tools/test_cam.py
@@ -0,0 +1,58 @@
+import cv2
+import numpy as np
+
+# Read from the camera
+cap = cv2.VideoCapture(2)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+# Set to YUYV to match the default on the usb_cam
+fourcc_cap = cv2.VideoWriter_fourcc(*"YUYV")
+cap.set(cv2.CAP_PROP_FOURCC, fourcc_cap)
+
+# Setup data collection
+cv2.namedWindow("Camera")
+
+# Read one image and get dimensions
+_, img = cap.read()
+h, w = img.shape[:2]
+
+#  Camera Matrix
+K = np.array(
+    [
+        [535.40740544, 0.0, 944.90571238],
+        [0.0, 536.25014079, 487.50675226],
+        [0.0, 0.0, 1.0],
+    ]
+)
+# K = np.array([  [553.7564405,   0.,              952.68355522],
+#                 [0.,            553.91867457,   495.19384415],
+#                 [0.,            0.,             1.        ]]
+# )
+
+# Distortion Matrix
+D = np.array([[-0.03568421], [-0.00683436], [-0.00039286], [0.00012972]])
+
+while True:
+    ret, img = cap.read()
+    # Wait for user input
+    k = cv2.waitKey(1)
+    if k % 256 == 27:  # ESC pressed
+        print("Escape hit, closing...")
+        break
+    if ret:
+        # undistort
+        map1, map2 = cv2.fisheye.initUndistortRectifyMap(
+            K, D, np.eye(3), K, (w, h), cv2.CV_16SC2
+        )
+        undistorted_img = cv2.remap(
+            img,
+            map1,
+            map2,
+            interpolation=cv2.INTER_LINEAR,
+            borderMode=cv2.BORDER_CONSTANT,
+        )
+        cv2.imshow("Camera", undistorted_img)
+
+
+# Closing all open windows
+cv2.destroyAllWindows()
