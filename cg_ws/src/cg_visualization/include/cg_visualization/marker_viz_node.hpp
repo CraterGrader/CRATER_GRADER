3bcd0daa03f97a7f379d87e88cf1a9a3187b76ee
CraterGrader <cratergrader@email.com>
2022-11-16 00:59:44 -0500

    first end to end with tool viz

--- cg_ws/src/cg_visualization/include/cg_visualization/marker_viz_node.hpp
+++ cg_ws/src/cg_visualization/include/cg_visualization/marker_viz_node.hpp
@@ -2,12 +2,10 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <visualization_msgs/msg/marker.hpp>
+#include <visualization_msgs/msg/marker_array.hpp>
 #include <cg_msgs/msg/encoder_telemetry.hpp>
-#include <cg_msgs/msg/actuator_command.hpp>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <cg_msgs/msg/actuator_state.hpp>
 #include <tf2/LinearMath/Quaternion.h>
-#include <nav_msgs/msg/odometry.hpp>
-#include <geometry_msgs/msg/pose_stamped.hpp>
 #include <math.h>
 
 namespace cg
@@ -22,24 +20,31 @@ namespace cg
 
     private:
       /* Publishers and Subscribers */
-      rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr tool_pose_pub_;
-      rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr steer_pose_pub_;
+      rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr tool_pose_pub_;
       rclcpp::Subscription<cg_msgs::msg::EncoderTelemetry>::SharedPtr enc_tele_sub_;
-      rclcpp::Subscription<cg_msgs::msg::ActuatorCommand>::SharedPtr act_cmd_sub_;
-      rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr pose_sub_;
+      rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr arrow_pub_;
 
       /* Callbacks */
       void updateToolViz(const cg_msgs::msg::EncoderTelemetry::SharedPtr msg);
-      void updateSteerViz(const cg_msgs::msg::ActuatorCommand::SharedPtr msg);
-      void poseUpdateViz(const nav_msgs::msg::Odometry::SharedPtr msg);
+      void timerCallback(); // For looping publish in idle mode
+      void actStateCallback(const cg_msgs::msg::ActuatorState::SharedPtr msg);
 
       // Tool Position Visualization
-      visualization_msgs::msg::Marker body, ground, tool1, tool2, tool_text;
-      geometry_msgs::msg::PoseStamped steer;
+      visualization_msgs::msg::Marker body, ground, tool1, tool2, tool_text, steer_angle_arrow_;
+      visualization_msgs::msg::MarkerArray tool_array;
+      rclcpp::TimerBase::SharedPtr timer_; // For looping publish rate
+      rclcpp::Subscription<cg_msgs::msg::ActuatorState>::SharedPtr act_state_sub_;
+      float tool_pose_ = 0.0;
+
       void toolPoseVizInit();
 
-      float tool_pose_ = 0.0;
-      double steer_pose_ = 0.0;
+      float arrow_scale_x_;
+      float arrow_scale_y_;
+      float arrow_scale_z_;
+      float arrow_r_;
+      float arrow_g_;
+      float arrow_b_;
+
     }; // class MarkerVizNode
   } // namespace cg_visualization
 } // namespace cg
--- cg_ws/src/cg_visualization/src/marker_viz_node.cpp
+++ cg_ws/src/cg_visualization/src/marker_viz_node.cpp
@@ -4,85 +4,81 @@ namespace cg {
 namespace cg_visualization
 {
   MarkerVizNode::MarkerVizNode() : Node("marker_viz_node") {
-
-    tool_pose_pub_ = this->create_publisher<visualization_msgs::msg::Marker>(
+    // Tool pose
+    tool_pose_pub_ = this->create_publisher<visualization_msgs::msg::MarkerArray>(
       "/viz/planning/tool_pose_viz", 1);
-    steer_pose_pub_ = this->create_publisher<geometry_msgs::msg::PoseStamped>(
-      "/viz/planning/steer_pose_viz", 1);
     enc_tele_sub_ = this->create_subscription<cg_msgs::msg::EncoderTelemetry>(
         "/encoder_telemetry", 1, std::bind(&MarkerVizNode::updateToolViz, this, std::placeholders::_1));
-    act_cmd_sub_ = this->create_subscription<cg_msgs::msg::EncoderTelemetry>(
-        "/actuator_cmd", 1, std::bind(&MarkerVizNode::updateSteerViz, this, std::placeholders::_1));
-    pose_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
-        "/odometry/filtered/ekf_global_node", 1, std::bind(&MarkerVizNode::poseUpdateViz,
-        this, std::placeholders::_1));
+    // Steer angle
+    arrow_pub_ = this->create_publisher<visualization_msgs::msg::Marker>(
+        "/visualization_marker", 1);
+    act_state_sub_ = this->create_subscription<cg_msgs::msg::ActuatorState>(
+        "/actuator/state", 1, std::bind(&MarkerVizNode::actStateCallback, this, std::placeholders::_1));
+    
+    // Timer callback
+    timer_ = this->create_wall_timer(
+        std::chrono::milliseconds(100),
+        std::bind(&MarkerVizNode::timerCallback, this));
+
+    RCLCPP_INFO(this->get_logger(), "In constructor");
+
+    // Arrow parameters
+    this->declare_parameter<float>("arrow_scale_x", 0.5);
+    this->get_parameter("arrow_scale_x", arrow_scale_x_);
+    this->declare_parameter<float>("arrow_scale_y", 0.05);
+    this->get_parameter("arrow_scale_y", arrow_scale_y_);
+    this->declare_parameter<float>("arrow_scale_z", 0.05);
+    this->get_parameter("arrow_scale_z", arrow_scale_z_);
+    this->declare_parameter<float>("arrow_r", 1.0);
+    this->get_parameter("arrow_r", arrow_r_);
+    this->declare_parameter<float>("arrow_g", 0.0);
+    this->get_parameter("arrow_g", arrow_g_);
+    this->declare_parameter<float>("arrow_b", 1.0);
+    this->get_parameter("arrow_b", arrow_b_);
 
     RCLCPP_INFO(this->get_logger(), "In Tool / Steer Viz Constructor");
 
     toolPoseVizInit();
   }
 
+  void MarkerVizNode::timerCallback()
+  {
+    // Publish both craters and the steering angle
+    arrow_pub_->publish(steer_angle_arrow_);
+    tool_array.clear();
+    tool_array.push_back(tool1);
+    tool_array.push_back(tool2);
+    tool_array.push_back(tool_text);
+    tool_array.push_back(body);
+    tool_array.push_back(ground);
+    tool_pose_pub_->publish(tool_array);
+  }
+
   void MarkerVizNode::updateToolViz(const cg_msgs::msg::EncoderTelemetry::SharedPtr msg)
   {
-    float msg_tool_pose = static_cast<float>(msg->tool_pos)/129.0;
-    float diff = std::fabs(tool_pose_ - msg_tool_pose);
-    float scaling = std::fabs(tool_pose_ + msg_tool_pose);
-    if (diff < std::numeric_limits<float>::epsilon() * scaling) {
-      tool_pose_ = msg_tool_pose;
+    RCLCPP_INFO(this->get_logger(), "In update tool viz");
+    float msg_tool_pose = static_cast<float>(msg->tool_pos)/328.0;
+    RCLCPP_INFO(this->get_logger(), std::to_string(msg_tool_pose).c_str());
+    tool_pose_ = msg_tool_pose;
 
-      // Update length and position of tool
-      tool1.pose.position.y = 2.5 - 0.5 * tool_pose_/80.0;
-      tool1.scale.y = tool_pose_/80.0;
-      tool2.pose.position.y = tool1.pose.position.y - tool1.scale.y/2;
-      tool_pose_pub_->publish(tool1);
-      tool_pose_pub_->publish(tool2);
-    }
-  }
-
-  void MarkerVizNode::updateSteerViz(const cg_msgs::msg::ActuatorCommand::SharedPtr msg)
-  {
-    steer_pose_ = msg->steer_position / 100.0 * 16.0 / 180.0 * M_PI;
-  }
-
-  void MarkerVizNode::poseUpdateViz(const nav_msgs::msg::Odometry::SharedPtr msg) {
-    // Set header and frame name
-    steer.header.stamp = this->get_clock()->now();
-    steer.header.frame_id = "base_link";
-
-    // Set the Pose relative to the robot frame to zeros
-    steer.pose.position.x = 0.0;
-    steer.pose.position.y = 0.0;
-    steer.pose.position.z = 0.0;
-
-    // Combine the current yaw and steer pose
-    tf2::Quaternion q_yaw, q_steer, q_new;
-    q_steer.setRPY(0.0, 0.0, steer_pose_);
-    tf2::convert(msg->pose.pose.orientation, q_yaw);
-    q_new = q_steer * q_yaw;
-    q_new.normalize();
-
-    // Set the quarternion in the message
-    steer.pose.orientation.x = q_new.x();
-    steer.pose.orientation.y = q_new.y();
-    steer.pose.orientation.z = q_new.z();
-    steer.pose.orientation.w = q_new.w();
-
-    // Publish the steering position
-    steer_pose_pub_->publish(steer);
+    // Update length and position of tool
+    tool1.pose.position.y = 2.25 - 0.25 * tool_pose_/80.0;
+    tool1.scale.y = 0.5 + 0.3 * tool_pose_/80.0;
+    tool2.pose.position.y = tool1.pose.position.y - tool1.scale.y/2;
   }
 
   void MarkerVizNode::toolPoseVizInit()
   {
-    // RCLCPP_INFO(this->get_logger(), "Starting Tool Pose Viz");
+    RCLCPP_INFO(this->get_logger(), "Tool pose viz initialization");
     // Tool Text Description
     tool_text.header.frame_id = "map";
     tool_text.header.stamp = this->get_clock()->now();
     tool_text.ns = "tool_text";
-    tool_text.id = 0;
+    tool_text.id = 1;
     tool_text.type = visualization_msgs::msg::Marker::TEXT_VIEW_FACING;
     tool_text.action = visualization_msgs::msg::Marker::ADD;
     tool_text.pose.position.x = -2.0;
-    tool_text.pose.position.y = 3.5;
+    tool_text.pose.position.y = 4.0;
     tool_text.pose.position.z = 0;
     tool_text.pose.orientation.x = 0.0;
     tool_text.pose.orientation.y = 0.0;
@@ -95,12 +91,12 @@ namespace cg_visualization
     tool_text.color.r = 0.0;
     tool_text.color.g = 0.0;
     tool_text.color.b = 0.0;
-    tool_text.text = "Tool Position";
+    tool_text.text = "Tool_Position";
     // Vehicle Body
     body.header.frame_id = "map";
     body.header.stamp = this->get_clock()->now();
     body.ns = "body";
-    body.id = 1;
+    body.id = 2;
     body.type = visualization_msgs::msg::Marker::CUBE;
     body.action = visualization_msgs::msg::Marker::ADD;
     body.pose.position.x = -2.0;
@@ -118,7 +114,7 @@ namespace cg_visualization
     ground.header.frame_id = "map";
     ground.header.stamp = this->get_clock()->now();
     ground.ns = "ground";
-    ground.id = 2;
+    ground.id = 3;
     ground.type = visualization_msgs::msg::Marker::CUBE;
     ground.action = visualization_msgs::msg::Marker::ADD;
     ground.pose.position.x = -2.0;
@@ -136,7 +132,7 @@ namespace cg_visualization
     tool1.header.frame_id = "map";
     tool1.header.stamp = this->get_clock()->now();
     tool1.ns = "tool1";
-    tool1.id = 3;
+    tool1.id = 4;
     tool1.type = visualization_msgs::msg::Marker::CUBE;
     tool1.action = visualization_msgs::msg::Marker::ADD;
     tool1.pose.position.x = -2.0;
@@ -153,7 +149,7 @@ namespace cg_visualization
     tool2.header.frame_id = "map";
     tool2.header.stamp = this->get_clock()->now();
     tool2.ns = "tool2";
-    tool2.id = 4;
+    tool2.id = 5;
     tool2.type = visualization_msgs::msg::Marker::CUBE;
     tool2.action = visualization_msgs::msg::Marker::ADD;
     tool2.pose.position.x = -2.2;
@@ -161,7 +157,7 @@ namespace cg_visualization
     tool2.pose.position.z = 0.1;
     tool2.pose.orientation = tool_text.pose.orientation;
     tool2.scale.x = 0.2;
-    tool2.scale.y = 0.5;
+    tool2.scale.y = 0.4;
     tool2.scale.z = 0.1;
     tool2.color = tool1.color;
 
@@ -172,5 +168,35 @@ namespace cg_visualization
     tool_pose_pub_->publish(tool2);
   }
 
+
+  void MarkerVizNode::actStateCallback(const cg_msgs::msg::ActuatorState::SharedPtr msg)
+  {
+    // Create arrow
+    steer_angle_arrow_.header.frame_id = "base_link";
+    steer_angle_arrow_.header.stamp = this->get_clock()->now();
+    steer_angle_arrow_.ns = "kinematics";
+    steer_angle_arrow_.id = 0;
+    steer_angle_arrow_.type = visualization_msgs::msg::Marker::ARROW;
+    steer_angle_arrow_.action = visualization_msgs::msg::Marker::ADD;
+    steer_angle_arrow_.pose.position.x = 0.2;
+    steer_angle_arrow_.pose.position.y = 0;
+    steer_angle_arrow_.pose.position.z = 0;
+
+    // rpy
+    tf2::Quaternion q;
+    q.setRPY(0, 0, -msg->steer_position); // Not sure why the steer angle needs to be negative
+    steer_angle_arrow_.pose.orientation.x = q.x();
+    steer_angle_arrow_.pose.orientation.y = q.y();
+    steer_angle_arrow_.pose.orientation.z = q.z();
+    steer_angle_arrow_.pose.orientation.w = q.w();
+
+    steer_angle_arrow_.scale.x = arrow_scale_x_;
+    steer_angle_arrow_.scale.y = arrow_scale_y_;
+    steer_angle_arrow_.scale.z = arrow_scale_z_;
+    steer_angle_arrow_.color.a = 1.0; // Don't forget to set the alpha!
+    steer_angle_arrow_.color.r = arrow_r_;
+    steer_angle_arrow_.color.g = arrow_g_;
+    steer_angle_arrow_.color.b = arrow_b_;
+  }
 } // namespace cg_visualization
 } // namespace cg
