90f2ec0c8f5344f94c6f740c39f373f695511e86
Alex Pletta <alexpletta@gmail.com>
2022-11-20 22:58:44 -0500

    manual map generation

--- tools/image_to_map/image_to_map_csv.py
+++ tools/image_to_map/image_to_map_csv.py
@@ -10,7 +10,7 @@ Converts a pixel image to csv format for a Map object
 - Uncomment the plt. lines below to show the image; file will be written after closing window
 """
 # Set these parameters
-input_filepath = '/root/CRATER_GRADER/tools/image_to_map/data/cg_craters_50x50.png'
+input_filepath = '/root/CRATER_GRADER/tools/image_to_map/data/cmu_craters_smoothed.png'
 resolution = 0.1
 
 # Append for output filename
@@ -20,15 +20,15 @@ output_filepath = input_filepath.split('.')[0] + '_map.csv'
 im = np.asarray(Image.open(input_filepath).convert('L'))
 
 # Scale to [-1,1]
-im = ((im / 254) - 0.5) * 2
+im = ((im / 254) - 0.5) * 2 * 0.1
 
 # Set and pack data; row-major order for cell data
 height = im.shape[0]
 width = im.shape[1]
 map_data = np.concatenate([[height, width, resolution], np.flip(im, axis=0).flatten()])
 
-# plt.imshow(im, cmap='gray', vmin=-1, vmax=1)
-# plt.show()
+plt.imshow(im, cmap='gray', vmin=-1, vmax=1)
+plt.show()
 
 # Save to file
 np.savetxt(output_filepath, map_data, fmt='%f')
